4c63ebddf4d061dc5c16c149ecc9523b34d6401c
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-10-11 02:09:33 +0300

    Updates

---
+++ precache-manifest.1d0f96b7b4fe7a5d95b4694968e89805.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f27c3f2d8702f0b012e609dec6661c",
+    "url": "/HW20-redux/index.html"
+  },
+  {
+    "revision": "17931c7635ffd65ef8ef",
+    "url": "/HW20-redux/static/css/main.b5732c5e.chunk.css"
+  },
+  {
+    "revision": "0b08beb55c8d620c9c38",
+    "url": "/HW20-redux/static/js/2.21d56086.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HW20-redux/static/js/2.21d56086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17931c7635ffd65ef8ef",
+    "url": "/HW20-redux/static/js/main.6233c85a.chunk.js"
+  },
+  {
+    "revision": "03beaa8a9d3c92336b49",
+    "url": "/HW20-redux/static/js/runtime-main.ef334755.js"
+  },
+  {
+    "revision": "99243e5afd8fc82f32f8bc1780879f43",
+    "url": "/HW20-redux/static/media/1251845.99243e5a.svg"
+  },
+  {
+    "revision": "c6972b988fe0baf17c4269bb5494986a",
+    "url": "/HW20-redux/static/media/134932.c6972b98.svg"
+  },
+  {
+    "revision": "01b91a1bdd82e18961f3bc2f42918fc3",
+    "url": "/HW20-redux/static/media/929417.01b91a1b.svg"
+  },
+  {
+    "revision": "5d02c6df9a7a9c58e45a302106d19a9d",
+    "url": "/HW20-redux/static/media/Star-Wars-Revenge-of-the-Sith.5d02c6df.jpg"
+  },
+  {
+    "revision": "e597acabaa68ed548f509cddfb1ab741",
+    "url": "/HW20-redux/static/media/check-solid.e597acab.svg"
+  },
+  {
+    "revision": "bbe4080b345b42532fdfb202d35cbdd8",
+    "url": "/HW20-redux/static/media/star-wars-girl-power-6fca5925.bbe4080b.jpg"
+  }
+]);
